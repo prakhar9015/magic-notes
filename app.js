b0d1e9718bf1886c3ec78f2e3913bef6707f7726
prakhar9015 <prakharraj9015@gmail.com>
2024-03-06 11:55:36 +0530

    added title and content search options

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ showNotes()
 const addBtn = document.getElementById("addBtn")
 addBtn.addEventListener("click", function(e) {
     let addTxt = document.getElementById("addTxt")
+    let addTitle = document.getElementById("addTitle")
     let notes = localStorage.getItem("notes")
 
     if (notes == null) {
-        notesObj = [];
+        notesObj = []; // in this notesObj, where all the notes resides as a list
     } else {
         notesObj = JSON.parse(notes);
     }
 
-    notesObj.push(addTxt.value);
+    let myObj = {
+        title: addTitle.value,
+        text: addTxt.value
+    }
+
+    notesObj.push(myObj); // in this list, value written in textArea is pushed
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value = "";
     console.log(notesObj);
     showNotes()
 });
@@ -30,27 +37,27 @@ function showNotes(){
         notesObj = JSON.parse(notes);
     }
 
-    let html = ""
+    let html = "" // this is just a string
     notesObj.forEach( function(element, index) {
 
         html += `
 
             <div class="card my-2 mx-2 noteCard" style="width: 18rem;">
                 <div class="card-body">
-                  <h5 class="card-title">Note ${index + 1}</h5>
-                  <p class="card-text">${element}</p>
-                  <button id=${index} onclick="deleteNote(this.id)" class="btn btn-primary">Delete note</button>
+                  <h5 class="card-title">${element.title}</h5>
+                  <p class="card-text">${element.text}</p>
+                  <button id=${index} ondblclick="deleteNote(this.id)" class="btn btn-primary">Delete note</button>
                 </div>
             </div>
         `
     });
 
-    let notesElm = document.getElementById("notes")
+    let notesElm = document.getElementById("notes") // this is the div where , we display all the notes.
 
-    if (notesElm.length != 0){
+    if (notesObj.length != 0){
         notesElm.innerHTML = html
     } else {
-        notesElm.textContent = `Nothing to show`
+        notesElm.innerHTML = `Nothing to show... add some notes to display `
     }
 }
 
@@ -77,13 +84,25 @@ let search = document.getElementById("searchTxt")
 
 search.addEventListener("input", function(){
 
+    let dropDown = document.getElementById("dropdown")
+    console.log(dropDown.value)
+
     let inputVal = search.value.toLowerCase()
     // console.log(`Print: ${inputVal}`)
-
     let noteCards = document.getElementsByClassName("noteCard")
 
     Array.from(noteCards).forEach(function(element){
-        let cardTxt = element.getElementsByTagName("p")[0].innerText
+
+        let cardTxt;
+
+        if (dropDown.value == "title_search"){
+            cardTxt = element.getElementsByTagName("h5")[0].innerText
+            // console.log("searching from TITLE")
+        }else {
+            cardTxt = element.getElementsByTagName("p")[0].innerText
+            // console.log("searching from CONTENT")
+        }
+
         // console.log(cardTxt)
 
         if (cardTxt.includes(inputVal)){
